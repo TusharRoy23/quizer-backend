54505a75a1a83e1f0761ecc14c80d693c3128f4d
TusharRoy23 <risenboy23@gmail.com>
2025-10-29 18:29:02 +0100

    message length validation increased

--- src/modules/public/question/dto/question-generate-conversation-payload.dto.ts
+++ src/modules/public/question/dto/question-generate-conversation-payload.dto.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const QuestionGenerateConversationPayloadDto = z.object({
     message: z.string({
         required_error: 'message is required',
-    }).max(25, 'question must be at most 25 characters long'),
+    }).max(50, 'question must be at most 50 characters long'),
 });
 
 export type QuestionGenerateConversationPayloadType = z.infer<typeof QuestionGenerateConversationPayloadDto>;
